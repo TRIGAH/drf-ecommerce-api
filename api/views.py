1bca34fd8aa89d9aa6330609ce3439ccc271f9b8
Trigah <ijenrandy2@gmail.com>
2024-03-25 14:39:05 +0100

    api to get products

--- api/views.py
+++ api/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from storeapp.models import Product,Category
-
+from .serializers import CategorySerializer,ProductSerializer
 # Create your views here.
 
 @api_view(['GET'])
 def api_products(request):
     products = Product.objects.all()
-    
-    return Response({"message": "Hello, world!"})
+    serializer = ProductSerializer(products)
+    return Response(serializer.data)
