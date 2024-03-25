aa29ac94ad8fdfb72a78cd2e1b0f6542939cd729
Trigah <ijenrandy2@gmail.com>
2024-03-25 15:15:24 +0100

    serialize all fields from model

--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.http import HttpResponse, JsonResponse
 from storeapp.models import Product,Category
 from .serializers import CategorySerializer,ProductSerializer
 # Create your views here.
@@ -8,5 +9,5 @@ from .serializers import CategorySerializer,ProductSerializer
 @api_view(['GET'])
 def api_products(request):
     products = Product.objects.all()
-    serializer = ProductSerializer(products)
-    return Response(serializer.data)
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data, status=200)
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('AWS_SECRET_KEY')
+# SECRET_KEY = os.environ.get('AWS_SECRET_KEY')
+SECRET_KEY = "uyriutrureiortiiotr"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -81,23 +82,23 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ecom',
-        'USER': 'postgres',
-        'PASSWORD': 'database',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'ecom',
+#         'USER': 'postgres',
+#         'PASSWORD': 'database',
+#         'HOST': 'localhost',
+#         'PORT': '5432'
+#     }
+# }
 
 
 
