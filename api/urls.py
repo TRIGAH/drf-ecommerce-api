ac894830189e12a336d04dc800738ea7f30b15eb
Trigah <ijenrandy2@gmail.com>
2024-03-27 10:44:03 +0100

    get a product endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from api.views import api_products
+from api.views import api_products,api_category
 
 urlpatterns = [
     path('product/', api_products),
+    path('category/', api_category),
 ]
--- api/views.py
+++ api/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from django.http import HttpResponse, JsonResponse
 from storeapp.models import Product,Category
 from .serializers import CategorySerializer,ProductSerializer
 # Create your views here.
@@ -10,4 +9,18 @@ from .serializers import CategorySerializer,ProductSerializer
 def api_products(request):
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data, status=200)
+
+
+@api_view(['GET'])
+def api_product(request,pk):
+    product = get_object_or_404(Product,pk=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def api_category(request):
+    categorys = Category.objects.all()
+    serializer = CategorySerializer(categorys, many=True)
     return Response(serializer.data, status=200)
