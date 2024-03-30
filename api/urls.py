b7c19ca2bf9adf0561a0a67fa374e148788212e2
Trigah <ijenrandy2@gmail.com>
2024-03-30 14:59:10 +0100

    get items for a given cart

--- api/urls.py
+++ api/urls.py
@@ -11,10 +11,14 @@ router.register("carts", views.CartViewSet)
 product_router = routers.NestedDefaultRouter(router, 'products', lookup='product')
 product_router.register('reviews',views.ReviewViewSet, basename='product-reviews')
 
+cart_router = routers.NestedDefaultRouter(router, 'carts', lookup='cart')
+cart_router.register('cartitems',views.CartitemViewSet, basename='cart-cartitems')
+
 # urlpatterns = router.urls
 
 urlpatterns = [
     path('',include(router.urls)),
     path('',include(product_router.urls)),
+    path('',include(cart_router.urls)),
 
 ]
