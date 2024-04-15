6bfa46db3c6f1d42b6431dc80048de64b99f0001
Trigah <ijenrandy2@gmail.com>
2024-04-15 18:33:31 +0100

    integrate payment

--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 import uuid
+import requests
 from storeapp.models import Product,Category,Review,Cart,Cartitems,Profile
 from .serializers import *
 from rest_framework.response import Response
@@ -12,6 +13,51 @@ from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 from api.filter import ProductFilter
 # Create your views here.
+
+
+
+def initiate_payment(amount, email, order_id):
+    url = "https://api.flutterwave.com/v3/payments"
+    headers = {
+        "Authorization": f"Bearer {settings.FLW_SEC_KEY}"
+        
+    }
+    
+    data = {
+        "tx_ref": str(uuid.uuid4()),
+        "amount": str(amount), 
+        "currency": "USD",
+        "redirect_url": "http:/127.0.0.1:8000/api/orders/confirm_payment/?o_id=" + order_id,
+        "meta": {
+            "consumer_id": 23,
+            "consumer_mac": "92a3-912ba-1192a"
+        },
+        "customer": {
+            "email": email,
+            "phonenumber": "080****4528",
+            "name": "Yemi Desola"
+        },
+        "customizations": {
+            "title": "Pied Piper Payments",
+            "logo": "http://www.piedpiper.com/app/themes/joystick-v27/images/logo.png"
+        }
+    }
+    
+
+    try:
+        response = requests.post(url, headers=headers, json=data)
+        response_data = response.json()
+        return Response(response_data)
+    
+    except requests.exceptions.RequestException as err:
+        print("the payment didn't go through")
+        return Response({"error": str(err)}, status=500)
+
+
+
+
+
+
 
 class ProductsViewSet(ModelViewSet):
     queryset  = Product.objects.all()
